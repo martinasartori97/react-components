9287a62e57f2b6f0a2dbeb1a4d03c17a100738d5
martinasartori97 <martinasartori9@gmail.com>
2024-11-12 17:11:11 +0100

    edit js

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,5 +1,7 @@
 export default function AppFooter() {
     return (
-        <footer>footer</footer>
+        <footer>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus nesciunt quo ipsum quos neque placeat aspernatur aut magni tenetur eos excepturi vero repellendus consectetur vitae, accusantium fugiat, at cupiditate aliquam Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum rem ea asperiores voluptate maxime fugit non ex necessitatibus, debitis ab sint suscipit, minus magnam, cumque in? Aspernatur recusandae harum obcaecati!</p>
+        </footer>
     )
 }
